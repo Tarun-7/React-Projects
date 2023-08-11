835901534cd9287538e37105d3939787b1a0a917
Tarun Sunkara <tarun@Taruns-MacBook-Air.local>
2023-08-11 21:58:45 +0100

    Added navigation bar

---
+++ project-1/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import logo from '../images/logo.png'
+import styles from '../style.css'
+
+export default function Navbar(){
+    return (
+        <div className="navbar-items">
+            <img className="logo" src={logo} alt="logo" />
+            <h2> ReactFacts </h2>
+            <h3> React Course - Project 1</h3>
+        </div>
+    )
+}
