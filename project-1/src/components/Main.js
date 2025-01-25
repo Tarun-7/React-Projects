3adc2b53025dc4f5946e8ad9965f8e4d26f581ec
Tarun Sunkara <tarun@Taruns-MacBook-Air.local>
2025-01-25 19:10:39 +0000

    minor changes

--- project-1/src/components/Main.js
+++ project-1/src/components/Main.js
@@ -4,11 +4,11 @@ export default function Main(){
         <div className="body">
             <h1> Fun Facts about React </h1>
             <ol>
-                <li> Was first release in 2013 </li>
-                <li> Was orginally creted by Jordan Walke</li>
-                <li> Has well over 200k stars on Github</li>
-                <li> Is maintained by Meta</li>
-                <li> Powers thousands of enterprise apps including mobile apps</li>
+                <li> Was first release in 2013. </li>
+                <li> Was orginally creted by Jordan Walke. </li>
+                <li> Has well over 200k stars on Github. </li>
+                <li> Is maintained by Meta. </li>
+                <li> Powers thousands of enterprise apps including mobile apps. </li>
             </ol>
         </div>
     )
