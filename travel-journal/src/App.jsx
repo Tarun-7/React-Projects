0ce04b5defbc151aa8dcbd091054328ce3accef6
Tarun Sunkara <tarun@MACBOOKAIR-5D4E.station>
2025-01-30 23:37:54 +0000

    added travel-journal react app

---
+++ travel-journal/src/App.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+import Header from './components/Header';
+import Card from './components/EntryCard';
+
+export default function App() {
+  return (
+    <Fragment>
+        <Header />
+        <Card />
+    </Fragment>
+
+  )
+}
---
+++ travel-journal/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Globe from '../assets/globe.png';
+
+export default function Header() {
+
+    return (
+        <header>
+            <img src="../assets/globe.png" alt="globe" />
+            <h1>my travel journal.</h1>
+        </header>
+    )
+}
